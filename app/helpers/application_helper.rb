2022212f6644bf541521d3dc13049c0178c8bb8b
megatimranikram <megatimran96@gmail.com>
2020-09-08 23:00:31 +0800

    Chapter: 4&5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title page_title
     base_title = "Ruby on Rails Tutorial Sample App"
-    page_title.blank? ? base_title : "#{page_title} | #{base_title}"
+    page_title.present? ? [page_title, base_title].join(" | ") : base_title
   end
 end
