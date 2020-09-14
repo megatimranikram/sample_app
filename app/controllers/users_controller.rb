143502bbdde5fde57849d2daabce5db5b20f8ecc
megatimranikram <megatimran96@gmail.com>
2020-09-14 17:17:46 +0800

    Chapter 9: Advanced Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,12 +17,12 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the sample app!"
       redirect_to @user
     else
-      flash.now[:danger] = "Account registration failed"
+      flash[:danger] = "Account registration failed"
+      render :new
     end
   end
 
   private
-  
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
   end
